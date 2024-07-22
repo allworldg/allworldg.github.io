cb832dd43c97173e43f246191379fe0e71e32136
allworldg <944937881@qq.com>
2024-07-22 09:57:17 +0800

    update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://allworldg.github.io',
+    base:"/AstroBlog",
     integrations: [
         mdx(),
         sitemap(),
