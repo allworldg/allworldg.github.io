0b4e82df6b605ad0bf61735d757dcd2105f1e7f0
allworldg <944937881@qq.com>
2026-01-27 15:29:39 +0800

    feat(rss): add rss support

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,16 +1,21 @@
 import { getCollection } from 'astro:content';
 import rss from '@astrojs/rss';
 import { SITE_DESCRIPTION, SITE_TITLE } from '../constants';
+import { DateFormat } from '../utils';
 
 export async function GET(context) {
     const posts = await getCollection('blog');
+    console.log(posts[0].data.publishDate);
+    console.log(DateFormat(posts[0].data.publishDate));
     return rss({
         title: SITE_TITLE,
         description: SITE_DESCRIPTION,
         site: context.site,
         items: posts.map((post) => ({
-            ...post.data,
-            link: `/blog/${post.id}/`
+            title: post.data.title,
+            link: `/blog/${post.id}/`,
+            pubDate: post.data.publishDate,
+            description: post.data.excerpt
         }))
     });
 }
