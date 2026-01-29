7e257c3fff15ca77d7990c845c133223ab20b54c
allworldg <944937881@qq.com>
2026-01-29 16:26:36 +0800

    feat(vue): add vue

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,10 +6,12 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import vue from '@astrojs/vue';
+
 // https://astro.build/config
 export default defineConfig({
     site: 'https://allworldg.xyz',
-    integrations: [mdx(), sitemap()],
+    integrations: [mdx(), sitemap(), vue()],
 
     vite: {
         plugins: [tailwindcss()]
