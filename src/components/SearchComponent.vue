c5d36f55dd3d23c0b656e2c854308ecb20c0fe6a
allworldg <944937881@qq.com>
2026-02-03 14:54:55 +0800

    feat(search): finish search result style

--- src/components/SearchComponent.vue
+++ src/components/SearchComponent.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
 import type { CollectionEntry, SearchEntry } from '../types';
 import { BLOG_URL } from '../constants';
-import { computed, ref, watch } from 'vue';
+import { ref } from 'vue';
 import { Document, Index, type EnrichedDocumentSearchResults, type EnrichedResults } from 'flexsearch';
-import { idText } from 'typescript';
 import { remark } from 'remark';
 import strip from 'strip-markdown';
 import { FIELD_SEARCH_CONTENT, FIELD_SEARCH_EXCERPT, FIELD_SEARCH_TITLE } from '../constants';
@@ -37,7 +36,7 @@ function handleSearch() {
     const result = docSearch.search(searchText.value, {
         highlight: {
             template: '<b>$1</b>',
-            boundary: 200,
+            boundary: 150,
             ellipsis: '...'
         },
         enrich: true
@@ -57,20 +56,34 @@ init();
 </script>
 
 <template>
-    <div>
-        <input class="border-black border-2" v-model="searchText" />
-        <button @click="handleSearch">search</button>
-    </div>
-    <div>
-        <div v-for="(blog, _) in titleResult" :key="blog.id">
-            <a v-html="blog.highlight" class="" :href="`${BLOG_URL}/${blog.doc!.id}`"></a>
-            <div>{{ blog.doc?.excerpt }}</div>
+    <div class="mt-10">
+        <div class="flex justify-center flex-wrap">
+            <input class="border-black rounded-md border-2 max-w-[300px] w-full" v-model="searchText" @keyup.enter="handleSearch" />
+            <button class="bg-[#4d90fe] hover:bg-[#357ae8] ml-5 rounded-md px-7" @click="handleSearch">
+                <svg class="text-white" width="13" height="13" viewBox="0 0 13 13">
+                    <title>搜索</title>
+                    <path
+                        fill="currentColor"
+                        d="m4.8495 7.8226c0.82666 0 1.5262-0.29146 2.0985-0.87438 0.57232-0.58292 0.86378-1.2877 0.87438-2.1144 0.010599-0.82666-0.28086-1.5262-0.87438-2.0985-0.59352-0.57232-1.293-0.86378-2.0985-0.87438-0.8055-0.010599-1.5103 0.28086-2.1144 0.87438-0.60414 0.59352-0.8956 1.293-0.87438 2.0985 0.021197 0.8055 0.31266 1.5103 0.87438 2.1144 0.56172 0.60414 1.2665 0.8956 2.1144 0.87438zm4.4695 0.2115 3.681 3.6819-1.259 1.284-3.6817-3.7 0.0019784-0.69479-0.090043-0.098846c-0.87973 0.76087-1.92 1.1413-3.1207 1.1413-1.3553 0-2.5025-0.46363-3.4417-1.3909s-1.4088-2.0686-1.4088-3.4239c0-1.3553 0.4696-2.4966 1.4088-3.4239 0.9392-0.92727 2.0864-1.3969 3.4417-1.4088 1.3553-0.011889 2.4906 0.45771 3.406 1.4088 0.9154 0.95107 1.379 2.0924 1.3909 3.4239 0 1.2126-0.38043 2.2588-1.1413 3.1385l0.098834 0.090049z"
+                    ></path>
+                </svg>
+            </button>
         </div>
+        <div class="w-full max-w-[500px] mx-auto pt-8 wrap-break-word">
+            <div class="mt-6 first:mt-0" v-for="(blog, _) in titleResult" :key="blog.id">
+                <h2 class="text-lg">
+                    <a class="text-base-green" v-html="blog.highlight" :href="`${BLOG_URL}/${blog.doc!.id}`"></a>
+                </h2>
+                <div>{{ blog.doc?.excerpt }}</div>
+            </div>
 
-        <div v-for="(blog, index) in contentResult" :key="blog.id">
-            <a class="" :href="`${BLOG_URL}/${blog.doc!.id}`">{{ blog.doc!.title }}</a>
-            <div></div>
-            <div v-html="blog.highlight"></div>
+            <div class="mt-6 first:mt-0" v-for="(blog, _) in contentResult" :key="blog.id">
+                <h2 class="text-lg">
+                    <a class="text-base-green" :href="`${BLOG_URL}/${blog.doc!.id}`">{{ blog.doc!.title }}</a>
+                </h2>
+                <div></div>
+                <div v-html="blog.highlight"></div>
+            </div>
         </div>
     </div>
 </template>
